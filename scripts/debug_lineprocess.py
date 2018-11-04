ca2f53324faf118915c9ea276d7c5a870203bda8
Dirk Mueller <dmueller@suse.com>
2018-11-04 11:09:07 +0100

    debug_lineprocess: Handle more than one input file
    This makes it easier to look at the word/token statistics
    over a larger set of baseline files.
    
    Change-Id: I9a48337c936fefd9e02de13ee551a87bcf2ab122

--- scripts/debug_lineprocess.py
+++ scripts/debug_lineprocess.py
@@ -22,21 +22,22 @@ from logreduce.tokenizer import Tokenizer
 try:
     path = sys.argv[1]
 except IndexError:
-    print("usage: %s file" % sys.argv[0])
+    print("usage: %s [file]..." % sys.argv[0])
     exit(1)
 
 tokens_c = Counter()
 word_c = Counter()
 line_set = set()
-for line in open(path):
-    word_c.update(line.split())
-    tokens = Tokenizer.process(line)
-    tokens_c.update(tokens.split())
-    line = line.rstrip()
-    if line not in line_set and (line != tokens):
-        line_set.add(line)
-        print("  ", line)
-        print("-> %s" % tokens)
+for path in sys.argv[1:]:
+    for line in open(path):
+        word_c.update(line.split())
+        tokens = Tokenizer.process(line)
+        tokens_c.update(tokens.split())
+        line = line.rstrip()
+        if line not in line_set and (line != tokens):
+            line_set.add(line)
+            print("  ", line)
+            print("-> %s" % tokens)
 
 print("Total words: %d Total Tokens: %d" % (
         len(word_c), len(tokens_c)))
