f17ce60b734dfe01d8561dfe6b9070db7cd654a0
Tristan Cacqueray <tdecacqu@redhat.com>
2020-10-29 19:37:10 +0000

    process: use a functional argument for keep_file
    This change replace the legacy exclude_files/exclude_paths by a
    generic `keep_file` function. To keep things simple, this change
    also remove the ability to change exclude lines when loading
    a model.
    
    Change-Id: Iee91d5c3164803a8367a2853d97b7cd6782ca509

--- logreduce/tests/test_process.py
+++ logreduce/tests/test_process.py
@@ -10,7 +10,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import io
 import unittest
 import os
 import re
@@ -22,24 +21,13 @@ import logreduce.process
 class ProcessTests(unittest.TestCase):
     def test_model_save_load(self):
         # Create a model
-        clf = logreduce.process.Classifier(
-            exclude_files=["test.txt"], exclude_lines=["foo", "bar"]
-        )
+        clf = logreduce.process.Classifier(exclude_lines=["foo", "bar"])
         expected_exclude_lines = re.compile(r"|".join(["foo", "bar"]))
-        expected_exclude_files = ["test.txt"]
         assert clf.exclude_lines == expected_exclude_lines
-        assert clf.exclude_files == expected_exclude_files
         with tempfile.NamedTemporaryFile() as tmpfile:
             clf.save_file(tmpfile.name)
             clf = logreduce.process.Classifier.load_file(tmpfile.name)
             assert clf.exclude_lines == expected_exclude_lines
-            assert clf.exclude_files == expected_exclude_files
-
-            # Also test exclude can be overriden on load
-            clf = logreduce.process.Classifier.load_file(
-                tmpfile.name, exclude_files=["test.txt", "new.txt"]
-            )
-            assert clf.exclude_files == ["test.txt", "new.txt"]
 
     def test_process_diff(self):
         # Compare two python test files
@@ -59,47 +47,37 @@ class ProcessTests(unittest.TestCase):
             assert outliers[0][0] > 0, "license matched as anomaly"
 
         # Save model and reload the model
-        model = io.BytesIO()
-        model.name = ":memory:"
-        clf.save(model)
-        model.seek(0)
-        logreduce.process.Classifier.check(model)
-        # joblib load reset the seek for io bytes, bypass model check in test
-        model = io.BytesIO(model.read())
-        try:
-            from sklearn.externals import joblib
-        except ImportError:
-            # Recent sklearn library doesn't vendor joblib anymore
-            import joblib
-        clf = joblib.load(model)
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            clf.save_file(tmpfile.name)
+            clf = logreduce.process.Classifier.load_file(tmpfile.name)
 
-        # Re-use the model with another test file
-        target = os.path.join(os.path.dirname(baseline), "test_units.py")
-        for file_result in clf.test(target):
-            filename, filename_orig, model, outliers, test_time = file_result
-            assert os.path.basename(model.sources[0]) == "test_process.py"
-            assert filename == "test_units.py"
-            assert test_time > 0
-            assert len(outliers) > 0
-            assert isinstance(outliers[0][0], int), "line number wrong type"
-            assert isinstance(outliers[0][1], float), "distance wrong type"
-            assert isinstance(outliers[0][2], str), "line wrong type"
-            assert outliers[0][0] > 0, "license matched as anomaly"
+            # Re-use the model with another test file
+            target = os.path.join(os.path.dirname(baseline), "test_units.py")
+            for file_result in clf.test(target):
+                filename, filename_orig, model, outliers, test_time = file_result
+                assert os.path.basename(model.sources[0]) == "test_process.py"
+                assert filename == "test_units.py"
+                assert test_time > 0
+                assert len(outliers) > 0
+                assert isinstance(outliers[0][0], int), "line number wrong type"
+                assert isinstance(outliers[0][1], float), "distance wrong type"
+                assert isinstance(outliers[0][2], str), "line wrong type"
+                assert outliers[0][0] > 0, "license matched as anomaly"
 
-        # Test the process method
-        result = clf.process(target)
-        assert result["baselines"] == [__file__]
-        assert result["targets"] == [target]
-        assert "test_units.py" in result["files"]
-        file_info = result["files"]["test_units.py"]
-        assert result["models"]["test_process.py"].get("uuid") != ""
-        assert file_info["mean_distance"] > 0.0
-        assert file_info["mean_distance"] < 1.0
-        assert isinstance(file_info["lines"][0], str), "line wrong type"
-        scores = file_info["scores"]
-        assert isinstance(scores[0][0], int), "line number wrong type"
-        assert isinstance(scores[0][1], float), "distance wrong type"
-        assert scores[0][0] > 0, "license matched as anomaly"
+            # Test the process method
+            result = clf.process(target)
+            assert result["baselines"] == [__file__]
+            assert result["targets"] == [target]
+            assert "test_units.py" in result["files"]
+            file_info = result["files"]["test_units.py"]
+            assert result["models"]["test_process.py"].get("uuid") != ""
+            assert file_info["mean_distance"] > 0.0
+            assert file_info["mean_distance"] < 1.0
+            assert isinstance(file_info["lines"][0], str), "line wrong type"
+            scores = file_info["scores"]
+            assert isinstance(scores[0][0], int), "line number wrong type"
+            assert isinstance(scores[0][1], float), "distance wrong type"
+            assert scores[0][0] > 0, "license matched as anomaly"
 
     def test_process_exclude_lines(self):
         # Generate two log file
