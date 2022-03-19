3b9801a05466e37198bae06ce6ef6a0d0aba69ea
Tristan Cacqueray <tdecacqu@redhat.com>
2022-03-19 16:50:10 +0000

    Update nix shell using overlay for latest stable

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
-{ pkgs ? import (fetchTarball
-  "https://github.com/NixOS/nixpkgs/archive/5cf5cad0da6244da30be1b6da2ff3d44b6f3ebe5.tar.gz")
-  { } }:
-
 let
+  rust_overlay = (import (builtins.fetchTarball
+    "https://github.com/oxalica/rust-overlay/archive/a21c163919cd90dd67bcd03345fac5441e53cccc.tar.gz"));
+  pkgs = import (fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/5cf5cad0da6244da30be1b6da2ff3d44b6f3ebe5.tar.gz") {
+      overlays = [ rust_overlay ];
+    };
+
   # dependencies of the current python implementation
   python = pkgs.python39.withPackages (ps:
     with ps; [
@@ -20,5 +23,5 @@ let
       pkgs.blas
     ]);
   # dependencies for the new implementation
-  rust = [ pkgs.cargo pkgs.rustc pkgs.rustfmt pkgs.clippy ];
-in pkgs.mkShell { buildInputs = [ python ] ++ rust; }
+  rust = pkgs.rust-bin.stable.latest.default;
+in pkgs.mkShell { buildInputs = [ python rust ]; }
