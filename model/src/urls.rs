a58373afc30a819fbb673ed7f9a1166ff207e95d
Tristan Cacqueray <tdecacqu@redhat.com>
2022-05-25 12:12:37 +0000

    model: ensure source prefix doesn't include '/' prefix

--- model/src/urls.rs
+++ model/src/urls.rs
@@ -38,7 +38,7 @@ impl Source {
 
     #[tracing::instrument(level = "debug")]
     pub fn httpdir_iter(url: &Url) -> Box<dyn Iterator<Item = Result<Source>>> {
-        let base_len = url.as_str().trim_end_matches('/').len();
+        let base_len = url.as_str().trim_end_matches('/').len() + 1;
         // TODO: fix the httpdir cache to work with iterator
         let urls = match CACHE.httpdir_get(url) {
             Some(res) => res,
