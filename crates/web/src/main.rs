69220521ac6698c54a8d86634d8220b268188033
Tristan Cacqueray <tdecacqu@redhat.com>
2023-09-08 01:54:35 +0000

    Integrate gloo-net to make network request

--- crates/web/src/main.rs
+++ crates/web/src/main.rs
@@ -1,7 +1,30 @@
+use gloo_net::http::Request;
 use yew::prelude::*;
 
-#[function_component]
-fn App() -> Html {
+#[function_component(App)]
+fn app() -> Html {
+    let report = use_state(|| None);
+    {
+        let report = report.clone();
+        use_effect_with_deps(
+            move |_| {
+                let report = report.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_report: String = Request::get("/data.json")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .expect("string?");
+                    report.set(Some(fetched_report));
+                });
+                || ()
+            },
+            (),
+        );
+    }
+
     let counter = use_state(|| 0);
     let onclick = {
         let counter = counter.clone();
@@ -11,11 +34,17 @@ fn App() -> Html {
         }
     };
 
+    let report_str = match (*report).clone() {
+        Some(report) => report,
+        None => "loading...".to_string(),
+    };
+
     html! {
         <div>
             {"Hello !"}
             <button {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
+            <p>{ report_str }</p>
         </div>
     }
 }
