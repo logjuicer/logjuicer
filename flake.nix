f582e27dd56d787ba71e39f56c5fb043997dac19
Tristan Cacqueray <tdecacqu@redhat.com>
2023-09-11 17:52:23 +0000

    Replace fenix/naersk with crane

--- flake.nix
+++ flake.nix
@@ -4,29 +4,60 @@
 
   inputs = {
     # nixpkgs is tracking nixpkgs-unstable
-    nixpkgs.url = "github:NixOS/nixpkgs/3d6ebeb283be256f008541ce2b089eb5fb0e4e01";
-    naersk.url =
-      "github:nix-community/naersk/d9a33d69a9c421d64c8d925428864e93be895dcc";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
-    fenix.url =
-      "github:nix-community/fenix/6c9f0709358f212766cff5ce79f6e8300ec1eb91";
-    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/3d6ebeb283be256f008541ce2b089eb5fb0e4e01";
+
     flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane/8b4f7a4dab2120cf41e7957a28a853f45016bd9d";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url =
+        "github:oxalica/rust-overlay/46dbbcaf435b0d22b149684589b9b059f73f4ffc";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, naersk, fenix, flake-utils }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, crane, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        naersk-lib = naersk.lib."${system}";
-        logreduce = naersk-lib.buildPackage {
-          pname = "logreduce";
-          src = self;
-          nativeBuildInputs = with pkgs; [ openssl pkg-config ];
-          doCheck = true;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
 
-        # python toolchain
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "x86_64-unknown-linux-musl" "wasm32-unknown-unknown" ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        src = pkgs.lib.cleanSourceWith {
+          src = ./.; # The original, unfiltered source
+          filter = path: type:
+            (pkgs.lib.hasSuffix ".html" path) || (pkgs.lib.hasSuffix ".js" path)
+            || (pkgs.lib.hasSuffix ".css" path)
+            || (pkgs.lib.hasSuffix ".txt" path) ||
+            # Default filter from crane (allow .rs files)
+            (craneLib.filterCargoSources path type);
+        };
+
+        cli-info = {
+          src = src;
+          cargoExtraArgs = "--package=logreduce-cli";
+        } // craneLib.crateNameFromCargoToml {
+          cargoToml = ./crates/cli/Cargo.toml;
+        };
+        static-exe = craneLib.buildPackage (cli-info // {
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        });
+        exe = craneLib.buildPackage cli-info;
+
         python = pkgs.python39.withPackages (ps:
           with ps; [
             setuptools-rust
@@ -43,43 +74,21 @@
             pkgs.blas
           ]);
 
-        toolchain = fenix.packages.${system}.default.toolchain;
-
-        # static build
-        toolchain-musl = with fenix.packages.${system};
-          combine [
-            minimal.rustc
-            minimal.cargo
-            targets.x86_64-unknown-linux-musl.latest.rust-std
-          ];
-        naersk-musl-lib = naersk.lib.${system}.override {
-          cargo = toolchain-musl;
-          rustc = toolchain-musl;
-        };
-        static-exe = naersk-musl-lib.buildPackage {
-          pname = "logreduce";
-          src = self;
-
-          nativeBuildInputs = with pkgs; [ pkgsStatic.stdenv.cc ];
-
-          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-
-          # lorgeduce-httpdir test are broken with musl
-          doCheck = false;
-        };
         release = pkgs.runCommand "logreduce-release" { } ''
           echo Creating release tarball with ${static-exe}
           cd ${static-exe};
-          tar --owner=0 --group=0 --mode='0755' -cf - bin/ | ${pkgs.bzip2}/bin/bzip2 -9 > $out
+          tar --owner=0 --group=0 --mode='0755' -cf - bin/logreduce | ${pkgs.bzip2}/bin/bzip2 -9 > $out
           echo cp $out logreduce-x86_64-linux.tar.bz2
         '';
 
       in {
-        defaultPackage = logreduce;
-        apps.default = flake-utils.lib.mkApp { drv = logreduce; };
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ toolchain openssl pkg-config ];
+        defaultPackage = exe;
+        apps.default = flake-utils.lib.mkApp {
+          drv = exe;
+          name = "logreduce";
+        };
+        devShell = craneLib.devShell {
+          packages = with pkgs; [ cargo-watch ];
           LOGREDUCE_CACHE = "1";
           UPDATE_GOLDENFILES = "1";
         };
