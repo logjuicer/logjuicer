999ddafc1145c0f1301ab801a7912c33f7e7717a
Tristan Cacqueray <tdecacqu@redhat.com>
2023-09-28 18:37:56 +0000

    tokenizer: split on '.' when processing index name

--- crates/model/src/model.rs
+++ crates/model/src/model.rs
@@ -37,7 +37,7 @@ pub mod zuul;
 const MODEL_MAGIC: &str = "LGRD";
 
 // Remember to bump this value when changing the tokenizer or the vectorizer to avoid using incompatible models.
-const MODEL_VERSION: usize = 5;
+const MODEL_VERSION: usize = 6;
 
 /// The user input.
 #[derive(Debug, Serialize, Deserialize)]
