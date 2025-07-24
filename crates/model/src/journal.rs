59db7b5d340fc4e225274e1b11866d3b3ad70387
Tristan Cacqueray <tdecacqu@redhat.com>
2025-07-24 09:08:17 +0000

    journal: handle multi line entry

--- crates/model/src/journal.rs
+++ crates/model/src/journal.rs
@@ -3,39 +3,25 @@
 
 use bytes::Bytes;
 use lazy_static::lazy_static;
-use std::io::Read;
+use std::io::{Read, Result};
 use systemd_journal_reader::JournalReader;
 use time::UtcDateTime;
 
 pub struct JournalLines<R: Read> {
     journal: JournalReader<R>,
+    remaining: Option<(usize, Bytes)>,
     pos: usize,
 }
 
 impl<R: Read> Iterator for JournalLines<R> {
-    type Item = std::io::Result<(Bytes, usize)>;
+    type Item = Result<(Bytes, usize)>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.pos += 1;
-        match self.journal.next_entry() {
-            None => None,
-            Some(entry) => {
-                let msg: String = entry
-                    .fields
-                    .get("MESSAGE")
-                    .and_then(|msg| {
-                        entry
-                            .fields
-                            .get("SYSLOG_IDENTIFIER")
-                            .or(entry.fields.get("_COMM"))
-                            .and_then(|sid| {
-                                format_ts(entry.realtime as i128)
-                                    .map(|ts| format!("{ts} - {sid}: {msg}"))
-                            })
-                    })
-                    .unwrap_or("".into());
-                Some(Ok((Bytes::from(msg), self.pos)))
-            }
+        if let Some(remaining) = self.remaining.take() {
+            self.next_remaining(false, remaining.0, remaining.1)
+        } else {
+            self.next_entry()
         }
     }
 }
@@ -53,9 +39,72 @@ fn format_ts(ts: i128) -> Option<String> {
         .and_then(|ts| ts.format(&FMT).ok())
 }
 
+fn is_multiline(mut line: Bytes) -> std::result::Result<Bytes, (Bytes, Bytes)> {
+    if let Some(pos) = line.iter().position(|c| c == &b'\n') {
+        let rest = line.split_off(pos);
+        Err((line, rest.slice(1..rest.len())))
+    } else {
+        Ok(line)
+    }
+}
+
 impl<R: Read> JournalLines<R> {
-    pub fn new(reader: R) -> std::io::Result<JournalLines<R>> {
+    pub fn new(reader: R) -> Result<JournalLines<R>> {
         let journal = JournalReader::new(reader)?;
-        Ok(JournalLines { journal, pos: 0 })
+        Ok(JournalLines {
+            journal,
+            pos: 0,
+            remaining: None,
+        })
+    }
+    fn next_remaining(
+        &mut self,
+        first: bool,
+        prefix: usize,
+        remaining: Bytes,
+    ) -> Option<Result<(Bytes, usize)>> {
+        let line = match is_multiline(remaining) {
+            Ok(line) => {
+                self.remaining = None;
+                line
+            }
+            Err((line, remaining)) => {
+                self.remaining = Some((prefix, remaining));
+                line
+            }
+        };
+        let line = if first {
+            line
+        } else {
+            let mut buf = Vec::with_capacity(prefix + line.len());
+            buf.resize(prefix, b' ');
+            buf.extend_from_slice(&line);
+            Bytes::from(buf)
+        };
+        Some(Ok((line, self.pos)))
+    }
+    fn next_entry(&mut self) -> Option<Result<(Bytes, usize)>> {
+        match self.journal.next_entry() {
+            None => None,
+            Some(entry) => {
+                let (prefix, msg): (usize, String) = entry
+                    .fields
+                    .get("MESSAGE")
+                    .and_then(|msg| {
+                        let msg = msg.trim_end_matches('\n');
+                        entry
+                            .fields
+                            .get("SYSLOG_IDENTIFIER")
+                            .or(entry.fields.get("_COMM"))
+                            .and_then(|sid| {
+                                format_ts(entry.realtime as i128).map(|ts| {
+                                    (ts.len() + sid.len() + 5, format!("{ts} - {sid}: {msg}"))
+                                })
+                            })
+                    })
+                    .unwrap_or((0, "".into()));
+                self.next_remaining(true, prefix, Bytes::from(msg))
+            }
+        }
     }
 }
